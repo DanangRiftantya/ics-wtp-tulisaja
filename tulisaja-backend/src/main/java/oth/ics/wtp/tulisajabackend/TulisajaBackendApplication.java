be3dfb6d36fec07834cc5cdbbff5f8445c8de47e
Danang Riftantya <danang1.riftantya@st.oth-regensburg.de>
2025-05-15 17:32:21 +0200

    Creation & setting up the backend environment (application properties + pom.xml)

---
+++ tulisaja-backend/src/main/java/oth/ics/wtp/tulisajabackend/TulisajaBackendApplication.java
@@ -0,0 +1,13 @@
+package oth.ics.wtp.tulisajabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulisajaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulisajaBackendApplication.class, args);
+	}
+
+}
---
+++ tulisaja-backend/src/test/java/oth/ics/wtp/tulisajabackend/TulisajaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oth.ics.wtp.tulisajabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulisajaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
