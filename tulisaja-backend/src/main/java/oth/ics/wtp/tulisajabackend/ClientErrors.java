3351fe0f8e661022ad05690cdb7016a857bf953d
Danang Riftantya <danang1.riftantya@st.oth-regensburg.de>
2025-05-19 20:19:30 +0200

    Creation of docker-compose.yml for the backend. Addition of ClientErrors.java and WeakCrypto.java

---
+++ tulisaja-backend/src/main/java/oth/ics/wtp/tulisajabackend/ClientErrors.java
@@ -0,0 +1,24 @@
+package oth.ics.wtp.tulisajabackend;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class ClientErrors {
+    private static final Logger logger = LoggerFactory.getLogger(ClientErrors.class);
+
+    public static ResponseStatusException userNameTaken(String name){
+        return log(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username is already taken: " + name));
+    }
+
+    public static ResponseStatusException invalidCredentials() {
+        return log(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid credentials"));
+    }
+
+    private static ResponseStatusException log(ResponseStatusException e) {
+        logger.error(ExceptionUtils.getMessage(e) + "\n" + ExceptionUtils.getStackTrace(e));
+        return e;
+    }
+}
--- tulisaja-backend/src/main/java/services/UserService.java
+++ tulisaja-backend/src/main/java/services/UserService.java
@@ -6,6 +6,8 @@ import entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.UserRepository;
+import oth.ics.wtp.tulisajabackend.ClientErrors;
+import oth.ics.wtp.tulisajabackend.WeakCrypto;
 
 @Service
 public class UserService {
